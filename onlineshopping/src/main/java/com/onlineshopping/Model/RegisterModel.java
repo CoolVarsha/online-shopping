e1d9c8384e29b5c3f3ef8afafca5d18df1cbf516
Varsha Pandey <28varsha26@gmail.com>
2018-01-30 18:38:33 -1100

    frontend complete

---
+++ onlineshopping/src/main/java/com/onlineshopping/Model/RegisterModel.java
@@ -0,0 +1,33 @@
+package com.onlineshopping.Model;
+
+import java.io.Serializable;
+
+import com.backendshopping.dto.Address;
+import com.backendshopping.dto.User;
+
+
+
+public class RegisterModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	private User user;
+	private Address billing;
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Address getBilling() {
+		return billing;
+	}
+	public void setBilling(Address billing) {
+		this.billing = billing;
+	}
+		
+}
